9034a15a9be2e57d7dcd3f9590dd5d90115e394c
mmmmayi <e0643891@u.nus.edu>
2023-02-10 18:56:47 +0800

    8s

--- IRM/run.sh
+++ IRM/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -o transCov_pos_adam_lr0.3_w0.95_hn0.92_n0.001_s8_th0.05_L_eluin.out
-#SBATCH --gres=gpu:2
-#SBATCH -w ttnusa1
+#SBATCH -o transCov_pos_8s_lr0.3_w0.95_hn0.92_n0.001_s8_th0.05_L_eluin.out
+#SBATCH --gres=gpu:1
+#SBATCH -w ttnusa10
 #SBATCH --cpus-per-task=16
-######SBATCH -p new
-TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --standalone --nnodes=1 --nproc_per_node=2 train.py
+#SBATCH -p new
+TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --standalone --nnodes=1 --nproc_per_node=1 train.py
 
--- IRM/train.py
+++ IRM/train.py
@@ -15,7 +15,7 @@ from Trainer.trainer import IRMTrainer
 import torch.distributed as dist
 from scheduler import ExponentialDecrease
 ## Set up project dir
-PROJECT_DIR = "exp/transCov_pos_adam_lr0.3_w0.95_hn0.92_n0.001_s8_th0.05_L_eluin"
+PROJECT_DIR = "exp/transCov_pos_8s_lr0.3_w0.95_hn0.92_n0.001_s8_th0.05_L_eluin"
 
 ## Config
 configs = {
@@ -28,11 +28,11 @@ configs = {
     "th": 0.05,
     "batchsize": 32,
     "data": 'noisy',
-    "dur": 4,
+    "dur": 8,
     "weight": 1000,
     "resume_epoch":None,
     "ratio":0.1,
-    "gpu":[0,1],
+    "gpu":[0],
     "optimizer": {
         "initial_lr": 0.3,
         "final_lr":0.0000003,
