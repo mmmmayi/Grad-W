21ffe4bd0ce097d34bae6d1d1e8c20c8b21f4905
mmmmayi <e0643891@u.nus.edu>
2023-06-16 16:43:08 +0800

    transCov_sclean_0.001_overall_detachC_noLM_encoder_weight_snr_norm_mae

--- IRM/run.sh
+++ IRM/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -o transCov_sclean_0.00001_overall_detachC_noLM_encoder_weight_snr_norm_mae.out
+#SBATCH -o transCov_sclean_0.001_overall_detachC_noLM_encoder_weight_snr_norm_mae.out
 #SBATCH --gres=gpu:1
-#SBATCH -w ttnusa12
+#SBATCH -w ttnusa11
 #SBATCH --cpus-per-task=8
 #SBATCH -p new
-TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --standalone --nnodes=1 --nproc_per_node=1 train.py
+TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --standalone --nnodes=1 --nproc_per_node=1 --rdzv_backend=c10d --rdzv_endpoint=$HOST_NODE_ADDR train.py
 
--- IRM/train.py
+++ IRM/train.py
@@ -15,7 +15,7 @@ from Trainer.trainer import IRMTrainer
 import torch.distributed as dist
 from scheduler import ExponentialDecrease
 ## Set up project dir
-PROJECT_DIR = "exp/transCov_sclean_0.00001_overall_detachC_noLM_encoder_weight_snr_norm_mae"
+PROJECT_DIR = "exp/transCov_sclean_0.001_overall_detachC_noLM_encoder_weight_snr_norm_mae"
 
 ## Config
 configs = {
@@ -33,8 +33,8 @@ configs = {
     "ratio":0.1,
     "gpu":[0],
     "optimizer": {
-        "initial_lr": 0.00001,
-        "final_lr": 0.00001,
+        "initial_lr": 0.001,
+        "final_lr": 0.001,
         "beta1": 0.0,
         "beta2": 0.9}}
 
