b39ab9d1701ff6a69ce3f6de672c75b4099abfec
mmmmayi <e0643891@u.nus.edu>
2023-10-26 14:00:55 +0800

    transCov_sclean_0.001_noLM_trueL_DFLdiffW_softmax_norelu_spatial_LSNR

--- IRM/run.sh
+++ IRM/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -o transCov_sclean_0.001_noLM_trueL_DFLdiffW_softmax1_norelu_spatial_LSNR.out
+#SBATCH -o transCov_sclean_0.001_noLM_trueL_DFLdiffW_softmax_norelu_spatial_LSNR.out
 #SBATCH --gres=gpu:1
 #SBATCH -w ttnusa11
 #SBATCH --cpus-per-task=8
